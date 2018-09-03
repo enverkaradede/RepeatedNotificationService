4c580e94e87a08742718194054c4746f31416793
enverkaradede <enverkaradede@gmail.com>
2018-09-03 13:43:41 +0300

    Arkaplanda dakikada bir bildirim gönderen bildirim servisi uygulaması

---
+++ app/src/main/java/com/example/enver/repeatednotification/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.enver.repeatednotification;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 5);
+
+        if(alarmManager != null)
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 1000 * 60, broadcast);
+
+    }
+}
